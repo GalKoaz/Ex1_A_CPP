50cccac7e10ad04160ead9625f3bb660bdd0ae2e
Gal <koazgal@gmail.com>
2022-03-06 12:36:23 +0200

    Upload

---
+++ mat.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Gal on 04/03/2022.
+//
+#include <iostream>
+#include "mat.hpp"
+
+namespace ariel{
+    std::string mat(int column, int row, char a, char b){
+    //here will be the code
+    }
+}
+
---
+++ mat.hpp
@@ -0,0 +1,11 @@
+//
+// Created by Gal on 04/03/2022.
+//
+
+#ifndef UNTITLED19_MAT_HPP
+#define UNTITLED19_MAT_HPP
+
+namespace ariel{
+    std::string mat(int column, int row, char a, char b);
+}
+#endif //UNTITLED19_MAT_HPP
